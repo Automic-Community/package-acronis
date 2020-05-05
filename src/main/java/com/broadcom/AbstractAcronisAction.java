6ea12e9c5be63211aaeddf64bf294d09a5bc16aa
Vijendra Parmar <vijendra.parmar@nagarro.com>
2020-05-05 17:09:26 +0530

    fixes for all actions

--- src/main/java/com/broadcom/AbstractAcronisAction.java
+++ src/main/java/com/broadcom/AbstractAcronisAction.java
@@ -21,7 +21,7 @@ public abstract class AbstractAcronisAction extends BaseAction {
 	@ActionInputParam(required = true, tooltip = "Provide the endpoint to connect to the acronis. E.g. https://sg-cloud.acronis.com", name = "UC4RB_AC_ENDPOINT", label = "Endpoint")
 	private String endpoint;
 
-	@ActionInputParam(required = true, tooltip = "Provide the version for the Acronis API. E.g. 2", name = "UC4RB_AC_VERSION", label = "Version")
+	@ActionInputParam(required = true, tooltip = "Provide the version for the Acronis API. E.g. 2", name = "UC4RB_AC_API_VERSION", label = "Version")
 	protected String version = Constants.ACRONIS_VERSION;
 
 	@ActionInputParam(required = true, tooltip = "Provide the username to connect to the acronis. E.g. SP1089871", name = "UC4RB_AC_USERNAME", label = "Username")
--- src/main/java/com/broadcom/UpdateTenantAction.java
+++ src/main/java/com/broadcom/UpdateTenantAction.java
@@ -19,7 +19,7 @@ import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 
 /**
- * This action will update a tenant.
+ * This action updates a tenant.
  * 
  * @author vishalkumar03
  *
@@ -27,39 +27,34 @@ import com.sun.jersey.api.client.WebResource;
 @Action(name = "UPDATE_TENANT", title = "Update Tenant", path = "ACRONIS")
 public class UpdateTenantAction extends AbstractAcronisAction {
 
-	@ActionInputParam(required = true, name = "UC4RB_AC_TENANT_ID", label = "Tenant Id", tooltip = "Provide the tenant id that you want to update. E.G. 9b81bf31-0c04-4f6e-8ebe-56900a3f9e76")
+	@ActionInputParam(required = true, name = "UC4RB_AC_TENANT_ID", label = "Tenant Id", tooltip = "Provide the tenant id that you want to update. E.g. 9b81bf31-0c04-4f6e-8ebe-56900a3f9e76")
 	String tenantId;
 
-	@ActionInputParam(required = true, name = "UC4RB_AC_TENANT_VERSION", label = "Current Version", tooltip = "Version should be same as the tenant that you want to update. E.G. 1")
-	Integer currentVersion = 1;
+	@ActionInputParam(required = true, name = "UC4RB_AC_TENANT_VERSION", label = "Current Version", tooltip = "Version should be same as the tenant that you want to update. E.g. 1")
+	Long currentVersion;
 
-	@ActionInputParam(name = "UC4RB_AC_TENANT_NAME", label = "Tenant Name", tooltip = "Provide the tenant name if you want to update. E.G. Test")
+	@ActionInputParam(name = "UC4RB_AC_TENANT_NAME", label = "Tenant Name", tooltip = "Provide the tenant name if you want to update. E.g. Test")
 	String tenantName;
 
-	@ActionInputParam(name = "UC4RB_AC_ENABLED", label = "Enabled", tooltip = "Enable or Disable the tenant. E.G. true")
+	@ActionInputParam(name = "UC4RB_AC_ENABLED", label = "Enabled", tooltip = "Enable or Disable the tenant. E.g. true")
 	Boolean enabled = true;
 
-	@ActionInputParam(name = "UC4RB_AC_FIRST_NAME", label = "First Name", tooltip = "Provide the first name if you want to update. E.G. Vishal")
+	@ActionInputParam(name = "UC4RB_AC_FIRST_NAME", label = "First Name", tooltip = "Provide the first name if you want to update. E.g. Vishal")
 	String firstName;
 
-	@ActionInputParam(name = "UC4RB_AC_LAST_NAME", label = "Last Name", tooltip = "Provide the last name if you want to update. E.G. Kumar")
+	@ActionInputParam(name = "UC4RB_AC_LAST_NAME", label = "Last Name", tooltip = "Provide the last name if you want to update. E.g. Kumar")
 	String lastName;
 
 	@ActionInputParam(name = "UC4RB_AC_EMAIL", label = "Email", tooltip = "Provide the email if you want to update. E.G. test@gmail.com")
 	String email;
 
 	@ActionOutputParam(name = "UC4RB_AC_NEW_VERSION")
-	Integer newVersion;
-
-	@Override
-	public void run() {
-		execute();
-	}
+	Long newVersion;
 
 	@Override
 	protected void executeSpecific() throws AcronisException {
-		Map<String, Object> request = createRequest();
 		ClientResponse response = null;
+		Map<String, Object> request = createRequest();
 		try {
 			WebResource webResource = client.resource(url);
 			webResource = webResource.path(Constants.API).path(version).path(Constants.TENANTS).path(tenantId);
@@ -69,13 +64,12 @@ public class UpdateTenantAction extends AbstractAcronisAction {
 			response = webResource.type(MediaType.APPLICATION_JSON).put(ClientResponse.class, request);
 			JsonObject jsonResponse = CommonUtil.jsonObjectResponse(response.getEntityInputStream());
 			ConsoleWriter.writeln("Response: " + CommonUtil.jsonPrettyPrinting(jsonResponse));
-			newVersion = jsonResponse.getInt(Constants.VERSION);
+			newVersion = jsonResponse.getJsonNumber(Constants.VERSION).longValue();
 		} catch (Exception e) {
-			String msg = String.format(Constants.REQ_ERROR_MESSAGE, url, e.getMessage());
-			// LOGGER.error(msg, e);
+			String msg = String.format(Constants.REQ_ERROR_MESSAGE, url);
+			LOGGER.warning(e.getMessage());
 			throw new AcronisException(msg, e);
 		}
-		// return new ActionResult(true, "The action executed Sucessfully \n");
 	}
 
 	/**
@@ -85,27 +79,28 @@ public class UpdateTenantAction extends AbstractAcronisAction {
 	 * @throws AcronisException
 	 */
 	private Map<String, Object> createRequest() throws AcronisException {
-		Map<String, Object> request = new HashMap<>();
-		request.put(Constants.VERSION, currentVersion);
-		request.put("enabled", enabled);
+
 		if (StringUtils.isEmpty(tenantId)) {
 			String msg = String.format(Constants.ISEMPTY, "Tenant Id");
-			// _log.error(msg);
 			throw new AcronisException(msg);
 		}
 
-		if (!StringUtils.isEmpty(tenantName)) {
+		Map<String, Object> request = new HashMap<>();
+		request.put(Constants.VERSION, currentVersion);
+		request.put("enabled", enabled);
+
+		if (StringUtils.isNotEmpty(tenantName)) {
 			request.put("tenantName", tenantName);
 		}
 
 		Map<String, Object> contactRequest = new HashMap<>();
-		if (!StringUtils.isEmpty(firstName)) {
+		if (StringUtils.isNotEmpty(firstName)) {
 			contactRequest.put("firstname", firstName);
 		}
-		if (!StringUtils.isEmpty(lastName)) {
+		if (StringUtils.isNotEmpty(lastName)) {
 			contactRequest.put("lastname", lastName);
 		}
-		if (!StringUtils.isEmpty(email)) {
+		if (StringUtils.isNotEmpty(email)) {
 			contactRequest.put("email", email);
 		}
 
