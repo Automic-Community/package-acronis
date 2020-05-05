e065f1481a90b124949b792b6c109f04e5868004
Vijendra Parmar <vijendra.parmar@broadcom.com>
2020-05-05 17:40:54 +0530

    few fixes

---
+++ src/main/java/com/broadcom/GetTenantAction.java
@@ -0,0 +1,99 @@
+package com.broadcom;
+
+import java.util.logging.Level;
+
+import javax.json.JsonObject;
+import javax.ws.rs.core.MediaType;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.broadcom.apdk.api.annotations.Action;
+import com.broadcom.apdk.api.annotations.ActionInputParam;
+import com.broadcom.apdk.api.annotations.ActionOutputParam;
+import com.broadcom.constants.Constants;
+import com.broadcom.exceptions.AcronisException;
+import com.broadcom.util.CommonUtil;
+import com.broadcom.util.ConsoleWriter;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+
+/**
+ * This action fetches the tenant info by using tenant id.
+ */
+@Action(name = "GET_TENANT", title = "Get Tenant", path = "ACRONIS")
+public class GetTenantAction extends AbstractAcronisAction {
+
+	/** tenantId. */
+	@ActionInputParam(name = "UC4RB_AC_TENANT_ID", label = "Tenant Id", required = true, tooltip = "Provide the Id of the tenant. E.g. 6f2e420b-bd8c-4ade-b3bb-4942d7c89032")
+	String tenantId;
+
+	/** tenantName. */
+	@ActionOutputParam(name = "UC4RB_AC_TENANT_NAME")
+	String tenantName;
+
+	/** tenantVersion. */
+	@ActionOutputParam(name = "UC4RB_AC_TENANT_VERSION")
+	Integer tenantVersion;
+
+	/** tenantEnabled. */
+	@ActionOutputParam(name = "UC4RB_AC_ENABLED")
+	Boolean tenantEnabled;
+
+	/** tenantParentId. */
+	@ActionOutputParam(name = "UC4RB_AC_PARENT_TENANT_ID")
+	String parentTenantId;
+
+	/** tenantKind. */
+	@ActionOutputParam(name = "UC4RB_AC_TENANT_KIND")
+	String tenantKind;
+
+	/**
+	 * This Method used to perform Get Tenant action specific operations.
+	 *
+	 * @throws AcronisException
+	 */
+	@Override
+	protected void executeSpecific() throws AcronisException {
+		validateInputs();
+		ClientResponse response = null;
+		try {
+			WebResource webResource = client.resource(url);
+			webResource = webResource.path("api").path(version).path("tenants").path(tenantId);
+			LOGGER.info("Calling url: " + webResource.getURI());
+			ConsoleWriter.writeln("Calling url: " + webResource.getURI());
+			webResource.accept(MediaType.APPLICATION_JSON);
+			response = webResource.get(ClientResponse.class);
+			prepareOutput(CommonUtil.jsonObjectResponse(response.getEntityInputStream()));
+		} catch (Exception ex) {
+			String msg = String.format(Constants.REQ_ERROR_MESSAGE, url);
+			LOGGER.log(Level.SEVERE, ex.toString(), ex);
+			throw new AcronisException(msg, ex);
+		}
+	}
+
+	private void validateInputs() throws AcronisException {
+		if (StringUtils.isEmpty(tenantId)) {
+			String msg = String.format(Constants.ISEMPTY, "Tenant Id");
+			throw new AcronisException(msg);
+		}
+	}
+
+	private void prepareOutput(JsonObject jsonObjectResponse) {
+		// write response to console
+		ConsoleWriter.writeln("Response: \n" + CommonUtil.jsonPrettyPrinting(jsonObjectResponse));
+		tenantName = jsonObjectResponse.getString("name");
+		parentTenantId = jsonObjectResponse.getString("parent_id");
+		tenantEnabled = jsonObjectResponse.getBoolean("enabled");
+		tenantKind = jsonObjectResponse.getString("kind");
+		tenantVersion = jsonObjectResponse.getInt("version");
+	}
+
+	/**
+	 * @return Action Name
+	 */
+	@Override
+	protected String getActionName() {
+		return "Get Tenant";
+	}
+
+}
---
+++ src/test/java/com/broadcom/GetTenantActionTest.java
@@ -0,0 +1,85 @@
+package com.broadcom;
+
+import java.io.ByteArrayInputStream;
+import java.net.URI;
+
+import javax.ws.rs.core.MediaType;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import com.broadcom.exceptions.AcronisException;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+
+/**
+ * This class Test the functionality of {@link GetTenantAction}
+ */
+public class GetTenantActionTest {
+	private Client client;
+	private WebResource webResource;
+	private ClientResponse response;
+	private WebResource.Builder builder;
+
+	@InjectMocks
+	private GetTenantAction customerTenantAction;
+
+	@BeforeEach
+	public void setupMock() {
+		client = Mockito.mock(Client.class);
+		webResource = Mockito.mock(WebResource.class);
+		response = Mockito.mock(ClientResponse.class);
+		builder = Mockito.mock(WebResource.Builder.class);
+		MockitoAnnotations.initMocks(this);
+	}
+
+	@Test
+	public void executeSpecificTest() throws AcronisException {
+		Mockito.when(client.resource(Mockito.any(URI.class))).thenReturn(webResource);
+		Mockito.when(webResource.path(Mockito.anyString())).thenReturn(webResource);
+		Mockito.when(webResource.accept(MediaType.APPLICATION_JSON)).thenReturn(builder);
+		Mockito.when(webResource.get(ClientResponse.class)).thenReturn(response);
+		Mockito.when(response.getEntityInputStream()).thenReturn(new ByteArrayInputStream(mockJsonData().getBytes()));
+		customerTenantAction.tenantId = "6f2e420b-bd8c-4ade-b3bb-4942d7c89032";
+		customerTenantAction.executeSpecific();
+
+		Mockito.verify(client, Mockito.times(1)).resource(Mockito.any(URI.class));
+		Mockito.verify(webResource, Mockito.times(1)).path("api");
+		Mockito.verify(webResource, Mockito.times(1)).path("tenants");
+		Mockito.verify(webResource, Mockito.times(1)).path("6f2e420b-bd8c-4ade-b3bb-4942d7c89032");
+		Mockito.verify(webResource, Mockito.times(1)).path("2");
+		Mockito.verify(webResource, Mockito.times(4)).path(Mockito.anyString());
+		Mockito.verify(webResource, Mockito.times(1)).accept(MediaType.APPLICATION_JSON);
+		Mockito.verify(webResource, Mockito.times(1)).get(ClientResponse.class);
+
+		Mockito.eq(customerTenantAction.tenantEnabled);
+		Assertions.assertEquals("cust001", customerTenantAction.tenantName);
+		Assertions.assertEquals(1, customerTenantAction.tenantVersion);
+		Assertions.assertEquals("79100dc2-757a-4261-a9a8-dac20a112e00", customerTenantAction.parentTenantId);
+		Assertions.assertEquals("customer", customerTenantAction.tenantKind);
+
+	}
+
+	private String mockJsonData() {
+		return "{\n" + "    \"enabled\": true,\n" + "    \"brand_id\": 1,\n"
+				+ "    \"id\": \"6f2e420b-bd8c-4ade-b3bb-4942d7c89032\",\n"
+				+ "    \"brand_uuid\": \"774a58d7-6ffd-11ea-b46d-001c42cdee0a\",\n" + "    \"internal_tag\": null,\n"
+				+ "    \"name\": \"cust001\",\n" + "    \"version\": 1,\n" + "    \"customer_id\": null,\n"
+				+ "    \"ancestral_access\": true,\n" + "    \"customer_type\": \"default\",\n" + "    \"contact\": {\n"
+				+ "        \"state\": null,\n" + "        \"city\": null,\n" + "        \"zipcode\": null,\n"
+				+ "        \"address2\": null,\n" + "        \"phone\": null,\n" + "        \"address1\": null,\n"
+				+ "        \"country\": null,\n" + "        \"firstname\": \"\",\n" + "        \"lastname\": \"\",\n"
+				+ "        \"email\": \"\"\n" + "    },\n" + "    \"owner_id\": null,\n"
+				+ "    \"language\": \"en-US\",\n" + "    \"update_lock\": {\n" + "        \"enabled\": false,\n"
+				+ "        \"owner_id\": null\n" + "    },\n"
+				+ "    \"parent_id\": \"79100dc2-757a-4261-a9a8-dac20a112e00\",\n"
+				+ "    \"default_idp_id\": \"11111111-1111-1111-1111-111111111111\",\n"
+				+ "    \"kind\": \"customer\",\n" + "    \"has_children\": true\n" + "}";
+	}
+
+}
