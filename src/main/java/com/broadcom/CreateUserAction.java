bc6b12b425622413c68125e090d67017cd723cb9
Hariom <hariom@nagarro.com>
2020-05-06 16:50:22 +0530

    Implementation of create user

--- src/main/java/com/broadcom/CreateUserAction.java
+++ src/main/java/com/broadcom/CreateUserAction.java
@@ -2,7 +2,21 @@ package com.broadcom;
 
 import com.broadcom.apdk.api.annotations.Action;
 import com.broadcom.apdk.api.annotations.ActionInputParam;
+import com.broadcom.apdk.api.annotations.ActionOutputParam;
+import com.broadcom.constants.Constants;
 import com.broadcom.exceptions.AcronisException;
+import com.broadcom.util.CommonUtil;
+import com.broadcom.util.ConsoleWriter;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.json.JsonObject;
+import javax.ws.rs.core.MediaType;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * This class used to create a user.
@@ -12,36 +26,97 @@ public class CreateUserAction extends AbstractAcronisAction {
 
     @ActionInputParam(name = "UC4RB_AC_TENANT_ID", required = true, tooltip = "Provide the Tenant id. E.g: "
             + "6f2e420b-bd8c-4ade-b3bb-4942d7c89032", label = "Tenant Id")
-    String tenantId;
+    private String tenantId;
 
     @ActionInputParam(name = "UC4RB_AC_USER_LOGIN", required = true, tooltip = "A login of the user account. E.g: "
             + "test@gmail.com", label = "Login")
-    String login;
+    private String login;
 
     @ActionInputParam(name = "UC4RB_AC_USER_EMAIL", required = true, tooltip =
             "A login of the user accountThe contact information of an account. Requires the email parameter. E.g: "
                     + "test@gmail.com", label = "Email")
-    String email;
+    private String email;
 
     @ActionInputParam(name = "UC4RB_AC_USER_FIRST_NAME", tooltip = "Provide the user fisrt name. E.g: test", label =
             "First Name")
-    String userFirstName;
+    private String userFirstName;
 
     @ActionInputParam(name = "UC4RB_AC_USER_LAST_NAME", tooltip = "Provide the user last name. E.g: test", label =
             "Last Name")
-    String userLASTName;
+    private String userLastName;
 
-    @ActionInputParam(name = "UC4RB_AC_USER_LANGUAGE", tooltip = "Language", label = "Language")
-    String language = "English";
+    @ActionOutputParam(name = "UC4RB_AC_USER_ID")
+    private String userId;
 
     @Override
     protected void executeSpecific() throws AcronisException {
+        ClientResponse response = null;
+        Map<String, Object> request = createRequest();
+        try {
+            WebResource webResource = client.resource(url);
+            webResource = webResource.path(Constants.API).path(version).path(Constants.USERS);
+            ConsoleWriter.writeln("Calling url: " + webResource.getURI());
+            ConsoleWriter.writeln("Request: " + request);
 
+            response = webResource.type(MediaType.APPLICATION_JSON).post(ClientResponse.class, request);
+            JsonObject jsonResponse = CommonUtil.jsonObjectResponse(response.getEntityInputStream());
+            ConsoleWriter.writeln("Response: " + CommonUtil.jsonPrettyPrinting(jsonResponse));
+            userId = jsonResponse.getString(Constants.USER_ID);
+        } catch (Exception e) {
+            String msg = String.format(Constants.REQ_ERROR_MESSAGE, url);
+            LOGGER.warning(e.getMessage());
+            throw new AcronisException(msg, e);
+        }
     }
 
     @Override
     protected String getActionName() {
         return null;
+    }
+
+    private boolean validateEmail(String email) {
+        Pattern pattern = Pattern.compile(Constants.EMAIL_VALIATION_REGEX);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    /**
+     * Validates the inputs and creates the request
+     *
+     * @return map with string keys and object type values
+     * @throws AcronisException
+     */
+    private Map<String, Object> createRequest() throws AcronisException {
+
+        if (StringUtils.isEmpty(tenantId)) {
+            String msg = String.format(Constants.ISEMPTY, "Tenant Id");
+            throw new AcronisException(msg);
+        }
+
+        if (!validateEmail(email)) {
+            String msg = String.format(Constants.INVALID_INPUT_PARAMETER, "Email", email);
+            throw new AcronisException(msg);
+        }
+
+        Map<String, Object> request = new HashMap<>();
+        request.put(Constants.TENANT_ID, tenantId);
+        request.put(Constants.LOGIN, login);
+
+        Map<String, Object> contactRequest = new HashMap<>();
+        if (StringUtils.isNotEmpty(userFirstName)) {
+            contactRequest.put(Constants.FIRST_NAME, userFirstName);
+        }
+        if (StringUtils.isNotEmpty(userLastName)) {
+            contactRequest.put(Constants.LAST_NAME, userLastName);
+        }
+        if (StringUtils.isNotEmpty(email)) {
+            contactRequest.put("email", email);
+        }
+
+        if (!contactRequest.isEmpty()) {
+            request.put("contact", contactRequest);
+        }
+        return request;
     }
 
 }
--- src/main/java/com/broadcom/constants/Constants.java
+++ src/main/java/com/broadcom/constants/Constants.java
@@ -4,37 +4,46 @@ import com.broadcom.apdk.api.annotations.EnumValue;
 
 /**
  * Class contains all the constants used in Automation engine java application.
- * 
  */
 public final class Constants {
-	/**
-	 * int constant for IO Buffer used to buffer the data.
-	 */
+    /**
+     * int constant for IO Buffer used to buffer the data.
+     */
 
-	public static final String HTTPS = "https";
-	public static final String ISEMPTY = "The %s can't be empty or null";
-	public static final String INVALID_INPUT_PARAMETER = "Invalid value for parameter [%s] : [%s]";
-	public static final String REQUEST_BODY_ERROR = "Exception occured in preparation of request body : %s";
-	public static final String REQ_ERROR_MESSAGE = "Exception occured while making request : [%s]";
-	public static final String RES_ERROR_MESSAGE = "Exception occured while parsing the response : %s";
-	public static final String ERROR_SKIPPING_CERT = "Exception occured while parsing the response : %s";
-	public static final String ERROR_MESSAGE = null;
-	public static final String ACTION_CATEGORY = "Ansible Tower";
-	public static final String ACRONIS_VERSION = "2";
-	public static final String UNABLE_TO_WRITE = "Unable to write at the given file path [%s]";
-	public static final String IGNORE_HTTPERROR = "IGNORE-HTTPERROR";
-	public static final String API = "api";
-	public static final String TENANTS = "tenants";
-	public static final String VERSION = "version";
+    public static final String HTTPS = "https";
+    public static final String ISEMPTY = "The %s can't be empty or null";
+    public static final String INVALID_INPUT_PARAMETER = "Invalid value for parameter [%s] : [%s]";
+    public static final String REQUEST_BODY_ERROR = "Exception occured in preparation of request body : %s";
+    public static final String REQ_ERROR_MESSAGE = "Exception occured while making request : [%s]";
+    public static final String RES_ERROR_MESSAGE = "Exception occured while parsing the response : %s";
+    public static final String ERROR_SKIPPING_CERT = "Exception occured while parsing the response : %s";
+    public static final String ERROR_MESSAGE = null;
+    public static final String ACTION_CATEGORY = "Ansible Tower";
+    public static final String ACRONIS_VERSION = "2";
+    public static final String UNABLE_TO_WRITE = "Unable to write at the given file path [%s]";
+    public static final String IGNORE_HTTPERROR = "IGNORE-HTTPERROR";
+    public static final String API = "api";
+    public static final String TENANTS = "tenants";
+    public static final String VERSION = "version";
+    public static final String USERS = "users";
+    public static final String TENANT_ID = "tenant_id";
+    public static final String USER_ID = "id";
+    public static final String LOGIN = "login";
+    public static final String EMAIL = "email";
+    public static final String FIRST_NAME = "firstname";
+    public static final String LAST_NAME = "lastname";
+    public static final String CONTACT = "contact";
 
-	public enum Kind {
-		@EnumValue("Customer")
-		CUSTOMER, @EnumValue("Partner")
-		PARTNER, @EnumValue("Folder")
-		FOLDER
-	}
+    /** EMAIL_VALIATION_REGEX used to validate the email. */
+    public static final String EMAIL_VALIATION_REGEX = "^(.+)@(.+)$";
 
-	private Constants() {
-	}
+    public enum Kind {
+        @EnumValue("Customer") CUSTOMER,
+        @EnumValue("Partner") PARTNER,
+        @EnumValue("Folder") FOLDER
+    }
+
+    private Constants() {
+    }
 
 }
