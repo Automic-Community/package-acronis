bbfd0bc9516738b9eb4e8623cef8cf9e03c4b795
Vishal Kumar <vishal.kumar03@nagarro.com>
2020-05-07 15:36:23 +0530

    made the current version optional

--- src/main/java/com/broadcom/DeleteUserAction.java
+++ src/main/java/com/broadcom/DeleteUserAction.java
@@ -2,12 +2,16 @@ package com.broadcom;
 
 import java.util.Objects;
 
+import javax.json.JsonObject;
+
 import org.apache.commons.lang3.StringUtils;
 
 import com.broadcom.apdk.api.annotations.Action;
 import com.broadcom.apdk.api.annotations.ActionInputParam;
 import com.broadcom.constants.Constants;
 import com.broadcom.exceptions.AcronisException;
+import com.broadcom.helper.GetUserHelper;
+import com.broadcom.util.CommonUtil;
 import com.broadcom.util.ConsoleWriter;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
@@ -18,7 +22,7 @@ public class DeleteUserAction extends AbstractAcronisAction {
 	@ActionInputParam(name = "UC4RB_AC_USER_ID", tooltip = "Provide the id of the user to be deleted. E.g. 88b24185-9b91-43d1-aa2c-b94665adcade8", required = true, label = "User Id")
 	String userId;
 
-	@ActionInputParam(name = "UC4RB_AC_USER_VERSION", tooltip = "Version should be same as the user that you want to delete. E.g. 1", required = true, label = "Current Version")
+	@ActionInputParam(name = "UC4RB_AC_USER_VERSION", tooltip = "Version should be same as the user that you want to delete. E.g. 1", label = "Current Version")
 	Long currentVersion;
 
 	@Override
@@ -39,13 +43,37 @@ public class DeleteUserAction extends AbstractAcronisAction {
 		}
 	}
 
+	/**
+	 * Validates the input provided by the user.
+	 * 
+	 * @throws AcronisException
+	 */
 	private void validateInputs() throws AcronisException {
 		if (StringUtils.isEmpty(userId))
 			throw new AcronisException(String.format(Constants.ISEMPTY, "User Id"));
 
 		if (Objects.isNull(currentVersion))
-			throw new AcronisException(String.format(Constants.ISEMPTY, "User Version"));
+			getCurrentVersion();
 
+	}
+
+	/**
+	 * Calls the get user API to get the current version only if user doesn't
+	 * provide the current version.
+	 * 
+	 * @throws AcronisException
+	 */
+	private void getCurrentVersion() throws AcronisException {
+		try {
+			ClientResponse response = GetUserHelper.urlCall(userId, client, url,
+					new String[] { Constants.API, version, "users" });
+			JsonObject jsonObject = CommonUtil.jsonObjectResponse(response.getEntityInputStream());
+			currentVersion = jsonObject.getJsonNumber(Constants.VERSION).longValue();
+		} catch (Exception e) {
+			String msg = String.format(Constants.REQ_ERROR_MESSAGE, url);
+			LOGGER.warning(e.getMessage());
+			throw new AcronisException(msg, e);
+		}
 	}
 
 	@Override
--- src/main/java/com/broadcom/GetUserAction.java
+++ src/main/java/com/broadcom/GetUserAction.java
@@ -1,7 +1,6 @@
 package com.broadcom;
 
 import javax.json.JsonObject;
-import javax.ws.rs.core.MediaType;
 
 import org.apache.commons.lang3.StringUtils;
 
@@ -10,10 +9,10 @@ import com.broadcom.apdk.api.annotations.ActionInputParam;
 import com.broadcom.apdk.api.annotations.ActionOutputParam;
 import com.broadcom.constants.Constants;
 import com.broadcom.exceptions.AcronisException;
+import com.broadcom.helper.GetUserHelper;
 import com.broadcom.util.CommonUtil;
 import com.broadcom.util.ConsoleWriter;
 import com.sun.jersey.api.client.ClientResponse;
-import com.sun.jersey.api.client.WebResource;
 
 @Action(name = "GET_USER", title = "Get User", path = "ACRONIS")
 public class GetUserAction extends AbstractAcronisAction {
@@ -38,10 +37,8 @@ public class GetUserAction extends AbstractAcronisAction {
 		ClientResponse response = null;
 		try {
 			validateInputs();
-			WebResource webResource = client.resource(url);
-			webResource = webResource.path("api").path(version).path("users").path(userId);
-			LOGGER.info("Calling url: " + webResource.getURI());
-			response = webResource.accept(MediaType.APPLICATION_JSON).get(ClientResponse.class);
+			response = GetUserHelper.urlCall(userId, client, url, new String[] { Constants.API, version, "users" });
+//			response = webResource.accept(MediaType.APPLICATION_JSON).get(ClientResponse.class);
 		} catch (Exception e) {
 			String msg = String.format(Constants.REQ_ERROR_MESSAGE, url);
 			LOGGER.warning(e.getMessage());
