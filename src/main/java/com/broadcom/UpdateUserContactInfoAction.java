117dfbcedce9656a425b27b57b35fb7c546bb989
Hariom <hariom@nagarro.com>
2020-05-08 13:42:10 +0530

    Merge create user and update user actions

--- src/main/java/com/broadcom/UpdateUserContactInfoAction.java
+++ src/main/java/com/broadcom/UpdateUserContactInfoAction.java
@@ -19,50 +19,62 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * This class used to create a user.
+ * This action class used to update the existing user contact info.
  */
-@Action(title = "Create User", name = "CREATE_USER", path = "ACRONIS")
-public class CreateUserAction extends AbstractAcronisAction {
+@Action(name = "UPDATE_USER_CONTACT_INFO", title = "Update User Contact Info", path = "ACRONIS")
+public class UpdateUserContactInfoAction extends AbstractAcronisAction {
 
-    @ActionInputParam(name = "UC4RB_AC_TENANT_ID", required = true, tooltip = "Provide the Tenant id. E.g: "
-            + "6f2e420b-bd8c-4ade-b3bb-4942d7c89032", label = "Tenant Id")
-    private String tenantId;
+    @ActionInputParam(name = "UC4RB_AC_USER_ID", required = true, tooltip = "Provide the User id. E.g: "
+            + "6f2e420b-bd8c-4ade-b3bb-4942d7c89032", label = "User Id")
+    private String userId;
 
-    @ActionInputParam(name = "UC4RB_AC_USER_LOGIN", required = true, tooltip =
-            "Provide login of the user account. E.g: " + "JohnDoe@gmail.com", label = "Login")
-    private String login;
-
-    @ActionInputParam(name = "UC4RB_AC_USER_EMAIL", required = true, tooltip =
-            "Provide contact email of the user. E.g: " + "JohnDoe@gmail.com", label = "Email")
+    @ActionInputParam(name = "UC4RB_AC_USER_EMAIL", tooltip = "Provide contact email of the user. E.g: "
+            + "johndoe@newmail.net", label = "Email")
     private String email;
 
-    @ActionInputParam(name = "UC4RB_AC_USER_FIRST_NAME", tooltip = "Provide the user first name. E.g: John", label =
+    @ActionInputParam(name = "UC4RB_AC_USER_FIRST_NAME", tooltip = "Provide the user fisrt name. E.g: foo", label =
             "First Name")
     private String userFirstName;
 
-    @ActionInputParam(name = "UC4RB_AC_USER_LAST_NAME", tooltip = "Provide the user last name. E.g: Doe", label =
+    @ActionInputParam(name = "UC4RB_AC_USER_LAST_NAME", tooltip = "Provide the user last name. E.g: bar", label =
             "Last Name")
     private String userLastName;
 
-    @ActionOutputParam(name = "UC4RB_AC_USER_ID")
-    private String userId;
+    @ActionInputParam(name = "UC4RB_AC_USER_VERSION", tooltip = "Provide the user current version. E.g: 2", label =
+            "User Version")
+    private long oldVersion;
 
-    @ActionOutputParam(name = "UC4RB_AC_USER_VERSION")
+    @ActionOutputParam(name = "UC4RB_AC_NEW_USER_VERSION")
     private long newVersion;
 
+    /**
+     * <ul>This method perform the tasks:
+     * <li>Call the Acronis update api.</li>
+     * <li>Update the user contact info.</li>
+     * <li>Throws the exception in case of error./li>
+     * </ul>
+     *
+     * @throws AcronisException
+     */
     @Override
     protected void executeSpecific() throws AcronisException {
         ClientResponse response = null;
         Map<String, Object> request = createRequest();
         try {
             WebResource webResource = client.resource(url);
-            webResource = webResource.path(Constants.API).path(version).path(Constants.USERS);
+            webResource = webResource.path(Constants.API).path(version).path(Constants.USERS).path(userId);
+            if (oldVersion > 0) {
+                this.newVersion = oldVersion;
+            } else {
+                this.newVersion = getVersion(webResource);
+            }
+
+            request.put("version", this.newVersion);
+            ConsoleWriter.writeln("Received User version: " + newVersion);
             ConsoleWriter.writeln("Calling url: " + webResource.getURI());
-            ConsoleWriter.writeln("Request: " + request);
 
-            response = webResource.type(MediaType.APPLICATION_JSON).post(ClientResponse.class, request);
+            response = webResource.type(MediaType.APPLICATION_JSON).put(ClientResponse.class, request);
             prepareOutput(CommonUtil.jsonObjectResponse(response.getEntityInputStream()));
-
         } catch (Exception e) {
             String msg = String.format(Constants.REQ_ERROR_MESSAGE, url);
             LOGGER.warning(e.getMessage());
@@ -70,18 +82,25 @@ public class CreateUserAction extends AbstractAcronisAction {
         }
     }
 
+    private long getVersion(WebResource webResource) {
+        ClientResponse response = webResource.type(MediaType.APPLICATION_JSON).get(ClientResponse.class);
+        JsonObject jsonResponse = CommonUtil.jsonObjectResponse(response.getEntityInputStream());
+        ConsoleWriter.writeln("Response: " + CommonUtil.jsonPrettyPrinting(jsonResponse));
+        return jsonResponse.getJsonNumber(Constants.VERSION).longValue();
+    }
+
     private void prepareOutput(JsonObject jsonObjectResponse) {
         // write response to console
         ConsoleWriter.writeln("Response: " + CommonUtil.jsonPrettyPrinting(jsonObjectResponse));
-        userId = jsonObjectResponse.getString(Constants.USER_ID);
         newVersion = jsonObjectResponse.getJsonNumber(Constants.VERSION).longValue();
     }
 
-    @Override
-    protected String getActionName() {
-        return "Create User";
-    }
-
+    /**
+     * This method used to validate the email.
+     *
+     * @param email
+     * @return
+     */
     private boolean validateEmail(String email) {
         Pattern pattern = Pattern.compile(Constants.EMAIL_VALIATION_REGEX);
         Matcher matcher = pattern.matcher(email);
@@ -96,8 +115,8 @@ public class CreateUserAction extends AbstractAcronisAction {
      */
     private Map<String, Object> createRequest() throws AcronisException {
 
-        if (StringUtils.isEmpty(tenantId)) {
-            String msg = String.format(Constants.ISEMPTY, "Tenant Id");
+        if (StringUtils.isEmpty(userId)) {
+            String msg = String.format(Constants.ISEMPTY, "User Id");
             throw new AcronisException(msg);
         }
 
@@ -107,9 +126,6 @@ public class CreateUserAction extends AbstractAcronisAction {
         }
 
         Map<String, Object> request = new HashMap<>();
-        request.put(Constants.TENANT_ID, tenantId);
-        request.put(Constants.LOGIN, login);
-
         Map<String, Object> contactRequest = new HashMap<>();
         if (StringUtils.isNotEmpty(userFirstName)) {
             contactRequest.put(Constants.FIRST_NAME, userFirstName);
@@ -125,6 +141,11 @@ public class CreateUserAction extends AbstractAcronisAction {
             request.put("contact", contactRequest);
         }
         return request;
+    }
+
+    @Override
+    protected String getActionName() {
+        return "Update User Contact Info";
     }
 
 }
