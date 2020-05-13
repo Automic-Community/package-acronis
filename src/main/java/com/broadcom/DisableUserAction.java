60520814ce9a29d994c23ca49ed1ba3b7ebbbdd2
Hariom <hariom@nagarro.com>
2020-05-13 14:01:42 +0530

    Incorporate CR defects

--- src/main/java/com/broadcom/DisableUserAction.java
+++ src/main/java/com/broadcom/DisableUserAction.java
@@ -15,6 +15,7 @@ import javax.json.JsonObject;
 import javax.ws.rs.core.MediaType;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * This class used to disable the user.
@@ -32,35 +33,52 @@ public class DisableUserAction extends AbstractAcronisAction {
     @ActionOutputParam(name = "UC4RB_AC_NEW_USER_VERSION")
     private long newVersion;
 
+    private boolean enabled;
+
     @Override
     protected void executeSpecific() throws AcronisException {
         ClientResponse response = null;
         Map<String, Object> request = createRequest();
+        if (enabled) {
+            try {
+                WebResource webResource = client.resource(url);
+                webResource = webResource.path(Constants.API).path(version).path(Constants.USERS).path(userId);
+                ConsoleWriter.writeln("Calling url: " + webResource.getURI());
+                response = webResource.type(MediaType.APPLICATION_JSON).put(ClientResponse.class, request);
+                prepareOutput(CommonUtil.jsonObjectResponse(response.getEntityInputStream()));
+            } catch (Exception e) {
+                String msg = String.format(Constants.REQ_ERROR_MESSAGE, url);
+                LOGGER.warning(e.getMessage());
+                throw new AcronisException(msg, e);
+            }
+        }
+    }
+
+    /**
+     * Calls the get tenant API to get the current version only if user doesn't
+     * provide the current version.
+     *
+     * @throws AcronisException
+     */
+    private void getCurrentVersion() throws AcronisException {
         try {
             WebResource webResource = client.resource(url);
             webResource = webResource.path(Constants.API).path(version).path(Constants.USERS).path(userId);
-            if (StringUtils.isBlank(oldVersion)) {
-                this.newVersion = getVersion(webResource);
-            } else {
-                this.newVersion = Long.parseLong(oldVersion);
+            LOGGER.info("Calling url: " + webResource.getURI());
+            ClientResponse clientResponse = webResource.type(MediaType.APPLICATION_JSON).get(ClientResponse.class);
+            JsonObject jsonResponseObject = CommonUtil.jsonObjectResponse(clientResponse.getEntityInputStream());
+            Long responseVersion = jsonResponseObject.getJsonNumber(Constants.VERSION).longValue();
+            if (Objects.nonNull(oldVersion) && !oldVersion.equals(responseVersion)) {
+                throw new AcronisException(Constants.TENANT_VERSION_MISMATCH);
             }
-            request.put("version", this.newVersion);
-            ConsoleWriter.writeln("Received User version: " + newVersion);
-            ConsoleWriter.writeln("Calling url: " + webResource.getURI());
-            response = webResource.type(MediaType.APPLICATION_JSON).put(ClientResponse.class, request);
-            prepareOutput(CommonUtil.jsonObjectResponse(response.getEntityInputStream()));
+
+            newVersion = responseVersion;
+            enabled = jsonResponseObject.getBoolean("enabled");
         } catch (Exception e) {
             String msg = String.format(Constants.REQ_ERROR_MESSAGE, url);
             LOGGER.warning(e.getMessage());
             throw new AcronisException(msg, e);
         }
-    }
-
-    private long getVersion(WebResource webResource) {
-        ClientResponse response = webResource.type(MediaType.APPLICATION_JSON).get(ClientResponse.class);
-        JsonObject jsonResponse = CommonUtil.jsonObjectResponse(response.getEntityInputStream());
-        ConsoleWriter.writeln("Response: " + CommonUtil.jsonPrettyPrinting(jsonResponse));
-        return jsonResponse.getJsonNumber(Constants.VERSION).longValue();
     }
 
     private void prepareOutput(JsonObject jsonObjectResponse) {
@@ -82,12 +100,13 @@ public class DisableUserAction extends AbstractAcronisAction {
             throw new AcronisException(msg);
         }
 
-        if (StringUtils.isNoneBlank(oldVersion) && !StringUtils.isNumeric(oldVersion)) {
+        if (StringUtils.isNotBlank(oldVersion) && !StringUtils.isNumeric(oldVersion)) {
             String msg = String.format(Constants.INVALID_INPUT_PARAMETER, "Current Version", oldVersion);
             throw new AcronisException(msg);
         }
-
+        getCurrentVersion();
         Map<String, Object> request = new HashMap<>();
+        request.put("version", this.newVersion);
         request.put("enabled", false);
         return request;
     }
--- src/main/java/com/broadcom/constants/Constants.java
+++ src/main/java/com/broadcom/constants/Constants.java
@@ -4,43 +4,42 @@ import com.broadcom.apdk.api.annotations.EnumValue;
 
 /**
  * Class contains all the constants used in Automation engine java application.
- * 
  */
 public final class Constants {
-	/**
-	 * int constant for IO Buffer used to buffer the data.
-	 */
+    /**
+     * int constant for IO Buffer used to buffer the data.
+     */
 
-	public static final String HTTPS = "https";
-	public static final String ISEMPTY = "The %s can't be empty or null";
-	public static final String INVALID_INPUT_PARAMETER = "Invalid value for parameter [%s] : [%s]";
-	public static final String REQUEST_BODY_ERROR = "Exception occured in preparation of request body : %s";
-	public static final String REQ_ERROR_MESSAGE = "Exception occured while making request : [%s]";
-	public static final String RES_ERROR_MESSAGE = "Exception occured while parsing the response : %s";
-	public static final String ERROR_SKIPPING_CERT = "Exception occured while parsing the response : %s";
-	public static final String ERROR_MESSAGE = null;
-	public static final String ACRONIS_VERSION = "2";
-	public static final String UNABLE_TO_WRITE = "Unable to write at the given file path [%s]";
-	public static final String IGNORE_HTTPERROR = "IGNORE-HTTPERROR";
-	public static final String API = "api";
-	public static final String TENANTS = "tenants";
-	public static final String VERSION = "version";
-	public static final String USERS = "users";
+    public static final String HTTPS = "https";
+    public static final String ISEMPTY = "The %s can't be empty or null";
+    public static final String INVALID_INPUT_PARAMETER = "Invalid value for parameter [%s] : [%s]";
+    public static final String REQUEST_BODY_ERROR = "Exception occured in preparation of request body : %s";
+    public static final String REQ_ERROR_MESSAGE = "Exception occured while making request : [%s]";
+    public static final String RES_ERROR_MESSAGE = "Exception occured while parsing the response : %s";
+    public static final String ERROR_SKIPPING_CERT = "Exception occured while parsing the response : %s";
+    public static final String ERROR_MESSAGE = null;
+    public static final String ACRONIS_VERSION = "2";
+    public static final String UNABLE_TO_WRITE = "Unable to write at the given file path [%s]";
+    public static final String IGNORE_HTTPERROR = "IGNORE-HTTPERROR";
+    public static final String API = "api";
+    public static final String TENANTS = "tenants";
+    public static final String VERSION = "version";
+    public static final String USERS = "users";
+    public static final String TENANT_VERSION_MISMATCH = "Tenant version does not match";
 
-	public enum Kind {
-		@EnumValue("Customer")
-		CUSTOMER, @EnumValue("Partner")
-		PARTNER, @EnumValue("Folder")
-		FOLDER, @EnumValue("Unit")
-		UNIT;
+    public enum Kind {
+        @EnumValue("Customer") CUSTOMER,
+        @EnumValue("Partner") PARTNER,
+        @EnumValue("Folder") FOLDER,
+        @EnumValue("Unit") UNIT;
 
-		@Override
-		public String toString() {
-			return name().toLowerCase();
-		}
-	}
+        @Override
+        public String toString() {
+            return name().toLowerCase();
+        }
+    }
 
-	private Constants() {
-	}
+    private Constants() {
+    }
 
 }
